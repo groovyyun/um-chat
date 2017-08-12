325a1ab073e0fb9fa1aa44ebd20f31248cbbb42a
우윤하 <woo@uyunhaui-MacBook-Air.local>
2017-08-12 20:13:53 +0900

    route 분리

---
+++ router/home.js
@@ -0,0 +1,9 @@
+module.exports = function(app){
+    var express = require('express');
+    var router = express.Router();
+
+    router.get('/', function(req, res){
+        res.render('login');
+    });
+    return router;
+};
